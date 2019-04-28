df9070a5a524fec84219d010368d0d8a9e3e1574
Alex Lee <hyunho98@gmail.com>
2019-04-28 00:55:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,8 +4,8 @@ function addElementToBeginningOfArray(array, element) {
   return [element,...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element);
+function destructivelyAddElementToBeginningOfArray(array, lement) {
+  return array.unshift(lement);
 }
 
 function addElementToEndOfArray(array, element) {
